0e8066708e0ed20493fbf5f2552beda6ecd78421
haad <haad@headbanggames.com>
2016-09-02 17:04:06 +0200

    Add logo

---
+++ logo.js
@@ -0,0 +1,13 @@
+// http://patorjk.com/software/taag/#p=display&f=Georgia11&t=Orbit
+
+module.exports = `
+                        ,,        ,,
+    .g8""8q.           *MM        db   mm
+  .dP'    \`YM.          MM             MM
+  dM'      \`MM \`7Mb,od8 MM,dMMb.\`7MM mmMMmm
+  MM        MM   MM' "' MM    \`Mb MM   MM
+  MM.      ,MP   MM     MM     M8 MM   MM
+  \`Mb.    ,dP'   MM     MM.   ,M9 MM   MM
+    \`"bmmd"'   .JMML.   P^YbmdP'.JMML. \`Mbmo
+
+`
